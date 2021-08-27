81c9c974c343566990014ef871b3017e609ab098
mfaqiri <mfaqiri33@gmail.com>
2021-08-27 16:20:58 -0400

    first commit

---
+++ sock-pairs/Pairs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace sock_pairs
+{
+    public class Pairs
+    {
+        public int PairCount(string s){
+        int count = 0;
+        string mutable = s;
+        while(mutable.Length > 0)
+        {
+            string next = mutable.Replace(mutable[0].ToString(),"");
+            count = (mutable.Length - next.Length);
+            mutable = next;
+        }
+
+        return count;
+
+        }
+    }
+}
